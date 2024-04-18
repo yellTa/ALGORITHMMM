e9fa091cb9bf0d57e4e6c75fded4e300eb1e26b0
yellTa <32352161+yellTa@users.noreply.github.com>
2024-04-18 19:40:57 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <algorithm>
-#include <map>
-#include <stack>
-#include <vector>
 
 using namespace std;
 
-//이분탐색 풀이
-int main(void) {
+int main (void) {
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
 
-    int n; cin >>n;
+    int n; cin >> n;
     int A[n];
-    for(int i=0; i< n; i++) {
-        cin>>A[i];
+    for(int i=0; i<n; i++) {
+        cin >> A[i];
     }
 
-    //sort 이분탐색을 위한 정렬
-    sort(A, A+n);
+    sort(A,A+n);
+    int m; cin>>m;
 
-    int m ; cin >>m;
-    map<int,int> B;
     for(int i=0; i<m; i++) {
-        int x; cin >>x;
-        cout << upper_bound(A,A+n,x) - lower_bound(A, A+n,x)<<" ";
+        int find; cin >>find;
+
+        cout << upper_bound(A, A+n, find) - lower_bound(A, A+n, find) << " ";
+
     }
 
 }
+
+
+

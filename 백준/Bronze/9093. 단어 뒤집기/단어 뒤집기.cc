868fa59472fd1570a169e443e952bc0617cc273d
yellTa <32352161+yellTa@users.noreply.github.com>
2024-05-08 17:56:30 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 24 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
@@ -1,36 +1,47 @@
-#include <stack>
 #include <iostream>
+#include <tuple>
+#include <stack>
 #include <queue>
+#include <algorithm>
+
+#define X first //세로
+#define Y second //가로
 
 using namespace std;
 
-int MA = 0;
-int tempA = 0;
-int answer = 0;
 
 int main() {
-    std::ios::sync_with_stdio(NULL);
-    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
 
-    int tc; cin >> tc;
+    int tc ; cin>>tc;
 
+    stack<char> stack;
     cin.ignore();
-    while(tc--){
-        string s; getline(cin, s);
-
-        stack<char> st;
+    while(tc--) {
+        string s; getline(cin,s);
         string answer="";
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] != ' ') {
+                stack.push(s[i]);
 
-        for(int i=0; i< s.size()+1; i++) {
-            if(s[i] == ' ' || i==s.size()) {
-                while(!st.empty()) {
-                    answer += st.top();
-                    st.pop();
+            }else if(s[i] == ' '){
+                while(!stack.empty()) {
+                    answer += stack.top(); stack.pop();
                 }
                 answer += " ";
-            }else st.push(s[i]);
-            if(i == s.size())break;
+            }
         }
-        cout << answer <<"\n";
+        while(!stack.empty()) {
+         answer += stack.top(); stack.pop();
+        }
+
+        cout << answer;
+        cout << "\n";
     }
+
+
+
+
+
 }

b1df6d5ebf3e37602f80226f98cdffb8d1ed8720
yellTa <32352161+yellTa@users.noreply.github.com>
2024-05-16 12:28:45 +0900

    [Gold III] Title: 불!, Time: 32 ms, Memory: 9984 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,81 +1,83 @@
-#include <iostream>
 #include <queue>
+#include <cmath>
+#include <iostream>
 
 #define X first
 #define Y second
+
 using namespace std;
 
+int fire[1001][1001];
+int jihun[1001][1001];
 
-string board[1002];
-int dist1[1002][1002];
-int dist2[1002][1002];
+int dx[4] = {-1, 1 ,0,0};
+int dy[4] = {0,0,-1,1};
 
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, 1, -1};
-
-int main(void) {
-std:
-    ios::sync_with_stdio(0);
+queue<pair<int,int>> FQ;
+queue<pair<int,int>> JQ;
+int main(){
+    std::ios::sync_with_stdio(0);
     std::cin.tie(0);
 
     int n,m; cin >>n >>m;
 
     for(int i=0; i<n; i++) {
-        fill(dist1[i], dist1[i] +m, -1);
-        fill(dist2[i], dist2[i] +m, -1);
-    }
+        string s; cin>>s;
+        for(int k=0; k<m; k++) {
+            if(s[k] == '#') {
+                fire[i][k] = -1;
+                jihun[i][k] = -1;
 
-    for(int i=0; i<n; i++) {
-        cin >> board[i];
-    }
-    queue<pair<int,int>> Q1;
-    queue<pair<int,int>> Q2;
+            }
+            if(s[k] == 'F') {
+                fire[i][k] = 1;
+                FQ.push({i,k});
+            }
+            if(s[k] == 'J') {
+                jihun[i][k] = 1;
+                JQ.push({i,k});
+            }
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            if(board[i][j] == 'F') {
-                Q1.push({i,j});
-                dist1[i][j] = 0;
-            }
-            if(board[i][j] == 'J') {
-                Q2.push({i,j});
-                dist2[i][j] = 0;
-            }
         }
     }
 
+    //fire bfs start
+    while(!FQ.empty()) {
+        auto cur = FQ.front(); FQ.pop();
 
-    //불 bfs
-    // 불에 대한 BFS
-    while(!Q1.empty()){
-        auto cur = Q1.front(); Q1.pop();
-        for(int dir = 0; dir < 4; dir++){
-            int nx = cur.X + dx[dir];
-            int ny = cur.Y + dy[dir];
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if(dist1[nx][ny] >= 0 || board[nx][ny] == '#') continue;
-            dist1[nx][ny] = dist1[cur.X][cur.Y]+1;
-            Q1.push({nx,ny});
+        for(int i=0; i<4; i++) {
+            int nx = cur.X + dx[i];
+            int ny = cur.Y + dy[i];
+
+            if(nx<0 || nx>=n || ny <0 || ny>=m)continue;
+            if(fire[nx][ny] || fire[nx][ny] == -1)continue;
+
+            FQ.push({nx,ny});
+            fire[nx][ny] = fire[cur.X][cur.Y] +1;
         }
     }
 
+    //jihun bfs start
+    while(!JQ.empty()) {
+        auto cur = JQ.front(); JQ.pop();
 
-    while(!Q2.empty()){
-        auto cur = Q2.front(); Q2.pop();
-        for(int dir = 0; dir < 4; dir++){
-            int nx = cur.X + dx[dir];
-            int ny = cur.Y + dy[dir];
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m){ // 범위를 벗어났다는 것은 탈출에 성공했다는 의미. 큐에 거리 순으로 들어가므로 최초에 탈출한 시간을 출력하면 됨.
-                cout << dist2[cur.X][cur.Y]+1;
+        for(int i=0; i<4; i++) {
+            int nx = cur.X + dx[i];
+            int ny = cur.Y + dy[i];
+            
+            if(nx<0 || nx>=n || ny <0 || ny>=m) {
+                cout << jihun[cur.X][cur.Y];
                 return 0;
             }
-            if(dist2[nx][ny] >= 0 || board[nx][ny] == '#') continue;
-            if(dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[cur.X][cur.Y]+1) continue; // 불의 전파 시간을 조건에 추가
-            dist2[nx][ny] = dist2[cur.X][cur.Y]+1;
-            Q2.push({nx,ny});
+            if(jihun[nx][ny] == -1 || jihun[nx][ny] !=0)continue;
+            if(jihun[cur.X][cur.Y] +1 >= fire[nx][ny] && fire[nx][ny] !=0) continue;
+            
+            
+            JQ.push({nx,ny});
+            jihun[nx][ny] = jihun[cur.X][cur.Y] +1;
         }
     }
 
-    cout << "IMPOSSIBLE";
 
+    cout << "IMPOSSIBLE";
 }

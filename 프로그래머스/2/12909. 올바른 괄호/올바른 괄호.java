84611372621087bed57bd7ba08b840000f9c16ca
yellTa <32352161+yellTa@users.noreply.github.com>
2024-05-23 21:00:43 +0900

    [level 2] Title: 올바른 괄호, Time: 53.74 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,8 +5,9 @@ import java.io.*;
 ( =1
 ) = -1
 
-0일때 -1들어오면 false
+닫힌 괄호가 시작으로 들어오면 이건 올바른 괄호가 될 수 없음
 
+0일때 -1들어오면 false
 마지막 수행 값이 0이 아니면 false
 
 */
@@ -21,6 +22,7 @@ class Solution {
         for(String x : A){
             if(x.equals("(") )result +=1;
             else if(x.equals(")")){
+                
                 if(result==0){
                     return false;
                 }

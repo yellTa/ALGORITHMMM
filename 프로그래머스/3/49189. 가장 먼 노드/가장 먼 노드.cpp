5381aced3ce0367451131ec95bcb0029e5bf7a22
yellTa <32352161+yellTa@users.noreply.github.com>
2024-06-13 20:55:26 +0900

    [level 3] Title: 가장 먼 노드, Time: 12.84 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -5,52 +5,38 @@
 using namespace std;
 
 int solution(int n, vector<vector<int>> edge) {
-   int answer=0;
-    
     vector<vector<int>> graph(n+1);
-    
-    for(int i=0; i< edge.size(); i++){
-        int from = edge[i][0];
-        int to = edge[i][1];
-        
-        graph[from].push_back(to);
-        graph[to].push_back(from);
+    vector<int> counts(n+1, 0);
+    vector<bool> visited(n+1, false);
+    queue<int> queue;
+
+    int answer = 0;
+    for (int i = 0; i < edge.size(); i++) {
+        graph[edge[i][0]].push_back(edge[i][1]);
+        graph[edge[i][1]].push_back(edge[i][0]);
     }
-    
-    //방문한 곳
-    vector<int> dist(n+1, -1); // n+1사이즈 초기화 -1로 채우기
-    queue<int> q;
-    
-    //시작점
-    dist[1] =0;
-    q.push(1);
-    
-    while(!q.empty())
-    {
-        int current = q.front();
-        q.pop();
-        
-        for(int next : graph[current])
-        {
-            if(dist[next] == -1)
-            {
-                dist[next] = dist[current] + 1;
-                q.push(next);
+
+    queue.push(1);
+    visited[1] = true;
+
+    while(!queue.empty()) {
+        int node = queue.front();
+        queue.pop();
+
+        for (int i = 0; i < graph[node].size(); i++) {
+            if (!visited[graph[node][i]]) {
+                int currentCount = counts[node] + 1;
+                visited[graph[node][i]] = true;
+                counts[graph[node][i]] = currentCount;
+                queue.push(graph[node][i]);
             }
         }
     }
-    
-    
-    int far = *max_element(dist.begin(), dist.end());
-    
-    for(int i=0; i<dist.size(); i++)
-    {
-        if(dist[i] == far)
-        {
-            answer++;
-        }
+
+    sort(counts.begin(), counts.end(), greater<int>());
+    for (auto cnt : counts) {
+        if (counts[0] != cnt) break;
+        answer++;
     }
-    
-    
     return answer;
 }

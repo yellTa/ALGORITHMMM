da1bfa53212fc6bc7fc93ffb200d96f0d9d9bcc7
yellTa <32352161+yellTa@users.noreply.github.com>
2024-07-05 12:38:04 +0900

    [Bronze II] Title: 카드 역배치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
@@ -4,14 +4,15 @@
 #include <set>
 #include <unordered_map>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-int A[21];
+vector<int> store(21);
 
-void reverse(int a, int b) {
-    for(int i=0; i < (b-a+1)/2; i++) {
-        swap(A[a+i], A[b-i]);
+void reverse(int x, int y) {
+    for(int i=0; i<((y-x+1)/2);  i++) {
+        swap(store[x+i], store[y-i]);
     }
 }
 
@@ -19,16 +20,16 @@ int main() {
     ios::sync_with_stdio(0);
     std::cin.tie(0);
 
-
-    for(int i=1 ;i<21; i++)A[i] =i;
-
-    for(int i=0 ;i<10; i++) {
-        int a, b; cin >>a>>b;
-
-        //역순 배치 로직
-        //1. 역배치 배열만들기
-        reverse(a,b);
+    for(int i=1; i<=20; i++) {
+        store[i] = i;
     }
-    for(int i=1; i<21; i++)cout << A[i] << " ";
+
+    for(int i=0; i<10; i++) {
+        int x,y; cin >> x>>y;
+
+        reverse(x,y);
+    }
+
+    for(int i=1; i<21; i++)cout << store[i] << " ";
 
 }

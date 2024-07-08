0a929f88c606ecc363119e09ea4ff897130c052a
yellTa <32352161+yellTa@users.noreply.github.com>
2024-07-09 00:30:11 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <vector>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+int n,m;
+int arr[10];// 선택한 숫자를 저장하는 배열
+bool isused[10]; //숫자가 이미 선택되언쓴지 여부를 저장하는 배열
+
+void func(int k) {
+    if(k==m) {
+        for(int i=0; i<m; i++)cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int st=1;
+    if(k !=0) st = arr[k-1]+1; //저장된 숫자 +1 -> 중복을 피하기 위함
+    for(int i=st; i<=n; i++) {
+        if(!isused[i]) {
+            isused[i] =1;
+            arr[k] = i;
+            func(k+1);
+            isused[i] = 0;
+        }
+    }
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n>>m;
+    func(0);
+}

3485bdbae43d75ba3c0e56046d94884975ad38fb
yellTa <32352161+yellTa@users.noreply.github.com>
2024-07-16 23:55:19 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -1,65 +1,66 @@
 #include <iostream>
 #include <algorithm>
+#include <queue>
 #include <set>
+#include <map>
 
 using namespace std;
 
-int l, c;
-char hint[20];
-int arr[20];
+//c 문자종류
+//l 비밀번호 자릿수
+int l,c;
+
+char words[20];
 int isused[20];
-
-bool check() {
-    int m=0;
-    int j=0;
-
-    string s;
-    for (int i = 0; i < l; i++) {
-
-        if (hint[arr[i]] == 'a' || hint[arr[i]] == 'i' ||
-            hint[arr[i]] == 'e' || hint[arr[i]] == 'o' || hint[arr[i]] == 'u') {
-            m++;
-        } else j++;
-        s +=hint[arr[i]];
-    }
-    if (m >= 1 && j >= 2) return true;
-
-    return false;
-}
-
+int arr[20];
 
 void func(int k) {
-    if (k == l) {
-        if (check()) {
-            for (int i = 0; i < l; i++)cout << hint[arr[i]];
-            cout << "\n";
+    if(k==l) {
+        // 자음 모음 체크 로직
+        int m=0; int j=0;
+        string s;
+
+        for(int i=0; i<l; i++) {
+            char c = words[arr[i]];
+            s += c;
+            if(c =='a' || c =='e' ||c =='i' || c == 'o'|| c =='u') {
+                m++;
+            }else j++;
         }
+
+        if(m>0 && j>1)cout <<s<< "\n";
+
         return;
     }
 
-    int st = 0;
-    if (k != 0)st = arr[k - 1] + 1;
+    int st=0;
+    if(k!=0) st= arr[k-1]+1;
 
-    for (int i = st; i < c; i++) {
-        if (!isused[i]) {
-            isused[i] = 1;
+    for(int i=st; i<c; i++) {
+        if(!isused[i]) {
+            isused[i] =1;
             arr[k] = i;
-            func(k + 1);
-            isused[i] = 0;
+
+            func(k+1);
+            isused[i] =0;
         }
     }
+
+
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> l >> c;
+    cin >> l>>c;
 
-    for (int i = 0; i < c; i++) {
-        cin >> hint[i];
+    for(int i=0; i< c; i++) {
+        cin >> words[i];
     }
+    sort(words, words+c);
 
-    sort(hint, hint + c);
     func(0);
+
+
 }

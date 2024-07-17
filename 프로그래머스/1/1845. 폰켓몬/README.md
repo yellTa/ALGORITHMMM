e0fecb6564d78dbe889915729dd3094a4e4e3d53
yellTa <32352161+yellTa@users.noreply.github.com>
2024-07-17 16:07:01 +0900

    [level 1] Title: 폰켓몬, Time: 1.15 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 4 MB, 시간: 0.28 ms
+메모리: 4.22 MB, 시간: 1.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 20일 20:31:38
+2024년 07월 17일 16:06:59
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,33 +1,26 @@
 #include <vector>
-#include <iostream>
 #include <set>
+
 using namespace std;
-/*
-set에 데이터 저장하기 
 
-n/2 >set.size() n/2출력하기
-
-n/2 <= set.size() set.size() 출력하기
-
-
-*/
 int solution(vector<int> nums)
 {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
     
-    int answer = 0;
-    set<int> s;
-    for(int x: nums){
-        s.insert(x);
+    set<int> set;
+    
+    
+    for(int i: nums){
+        set.insert(i);
     }
+    int m =set.size();
+    int k= nums.size()/2;
     
-    cout <<s.size();
-    
-    if(nums.size()/2 > s.size()){
-        answer = s.size();
-    }else answer = nums.size()/2;
-    
-    
-    return answer;
+    if(m<k){
+        return m;
+    }else{
+        return k;
+    }
+
 }

47af2b06dad0f5a4162f61c507eed41008b9eb88
yellTa <32352161+yellTa@users.noreply.github.com>
2024-09-09 11:21:32 +0900

    [Silver III] Title: 두 수의 합, Time: 240 ms, Memory: 28296 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,31 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.sql.SQLOutput;
+import java.util.*;
 
 class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int numbers = Integer.parseInt(br.readLine());
 
-        int[] store = new int[1000010];
+        int tc = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < numbers; i++) {
-            int value = Integer.parseInt(st.nextToken());
-            store[value] = 1;
+
+        int [] arr = new int[1000010];
+
+        for(int i=0; i<tc; i++){
+            int x = Integer.parseInt(st.nextToken());
+            arr[x] = 1;
         }
 
         int target = Integer.parseInt(br.readLine());
-        int answer = 0;
+        int answer=0;
 
-        // i는 target/2까지만 확인하면 됩니다.
-        for (int i = 1; i <= target / 2; i++) {
-            if (store[i] == 1 && target - i <= 1000000 && target - i > 0 && store[target - i] == 1 && i != target - i) {
+        for(int i=1; i<=target/2; i++){
+            if(arr[i] ==1 && target-i <=1000000 && target-i >0  && arr[target-i] ==1 && i  !=target-i){
                 answer++;
             }
         }
-
         System.out.println(answer);
     }
 }

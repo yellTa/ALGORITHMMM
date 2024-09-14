d639c5fd008240d682d8d8400571473f42494241
yellTa <32352161+yellTa@users.noreply.github.com>
2024-09-14 19:53:32 +0900

    [level 2] Title: 문자열 압축, Time: 26.87 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 80.7 MB, 시간: 3.86 ms
+메모리: 88.9 MB, 시간: 26.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 25일 12:05:44
+2024년 09월 14일 19:53:30
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -2,33 +2,63 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int answer = s.length();
+        int answer = 0;
+        //쪼개기 문자열 만들기
         
-        int count=1;
-        for(int i=1; i<=s.length()/2; i++){
+        String tempAnswer="";
+        
+        if(s.length()==1)return 1;
+        
+        Stack<String> stack = new Stack<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=1; i<=s.length()/2; i++){ //1번부터 length/2까지 쪼갤예정
+            //i는 쪼개는 숫자가 된다. 
+            //default값 지정
             
-            StringBuilder result = new StringBuilder();
-            
-            String start = s.substring(0,i);
-            
-            for(int k=i; k<=s.length(); k= k+i){
-                int end = Math.min(k+i, s.length());
+            for(int k=0; k<s.length(); k +=i){
+                int x = Math.min(k+i, s.length());
+                String temp = s.substring(k,x);
                 
-                String compare = s.substring(k, end);
-                
-                if(start.equals(compare)){
-                    count++;
-                }else{
-                    if(count>=2)result.append(count);
-                    result.append(start);
-                    start = compare;
-                    count=1;
+                if(stack.isEmpty()){
+                    stack.push(temp);
+                    continue;
                 }
+                if(stack.peek().equals(temp)){
+                    stack.push(temp);
+                    
+                }else{//다른게 들어온 경우
+                    if(!stack.isEmpty()){
+                        String word = stack.peek();
+                        int size = stack.size();
+                        
+                        if(size ==1){
+                            tempAnswer += word;
+                        }else{
+                            tempAnswer +=(stack.size())+word;
+                        }
+                    }
+                    
+                    stack.clear();
+                    stack.push(temp);
+                }
+                // System.out.print(s.substring(k,x)+ " "); 
             }
-            result.append(start);
-            answer = Math.min(answer,result.length());
+            //마지막을 Stack에 남은 값 까지 더해줌
+            if(!stack.isEmpty()){
+                String word = stack.peek();
+                int size = stack.size();
+                if(size ==1)tempAnswer +=word;
+                else tempAnswer +=(stack.size())+word;
+            }
+            stack.clear();
+            pq.add(tempAnswer.length());
             
+            tempAnswer="";
+            // System.out.println();
         }
-        return answer;
+        
+        
+        return pq.poll();
     }
 }

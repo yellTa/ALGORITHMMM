4a20ef2342d80a4a6a0e5e1eddd71a1aa8bfeca2
yellTa <32352161+yellTa@users.noreply.github.com>
2024-09-30 13:38:05 +0900

    [Silver IV] Title: solved.ac, Time: 320 ms, Memory: 37908 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,23 +1,22 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+
 
 class Main {
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
-
-        // Handle case when there are no elements
-        if (tc == 0) {
+        if(tc==0){
             System.out.println(0);
             return;
         }
-
         int[] store = new int[tc];
-        // 절사평균 구하기
-        int del = (int) Math.round(tc * 0.15);
+        //절사평균 구하기
+        int del = (int) Math.round(tc * (0.15));
 
         for (int i = 0; i < store.length; i++) {
             int x = Integer.parseInt(br.readLine());
@@ -25,14 +24,14 @@ class Main {
         }
         Arrays.sort(store);
 
-        int sum = 0;
-        for (int i = del; i < store.length - del; i++) {
-            sum += store[i];
+        int sum =0;
+        for(int i=del; i<= store.length-del-1; i++){
+            sum+=store[i];
         }
-
         // Calculating average as double to prevent integer division issues
         double average = (double) sum / (store.length - (del * 2));
         int answer = (int) Math.round(average);
         System.out.println(answer);
+
     }
 }
